30252ae048aa9d0f56ca21f367cf565fca9dcab4
gustavo-cigaran <gustavocigaran@gmail.com>
2026-04-02 10:37:07 -0300

    feat(auth): add login and logout view using django auth

--- flight_reservation/flight_reservation/urls.py
+++ flight_reservation/flight_reservation/urls.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path
 from core.views import flight_list, create_reservation, customer_list, show_flight_reservation
 
@@ -8,4 +9,6 @@ urlpatterns = [
     path('reserve/', create_reservation),
     path('customers/', customer_list),
     path('flights/<int:flight_id>/',show_flight_reservation),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout')
 ]
