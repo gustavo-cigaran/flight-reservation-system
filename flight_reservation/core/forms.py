1f327aafd75ddfa75bd1902c0023f98185e54350
gustavo-cigaran <gustavocigaran@gmail.com>
2026-04-05 12:31:21 -0300

    feat(reservation): add airplane capacity validation

--- flight_reservation/core/forms.py
+++ flight_reservation/core/forms.py
@@ -1,10 +1,25 @@
 from django import forms
+from django.core.exceptions import ValidationError
 from .models import Reservation, Airplane
 
 class ReservationForm(forms.ModelForm):
     class Meta:
         model = Reservation
         fields = ['customer', 'flight', 'seat_number']
+
+    def clean(self):
+        cleaned_data = super().clean()
+
+        flight = cleaned_data.get('flight')
+
+        if flight:
+            reservations = Reservation.objects.filter(flight=flight)
+            if self.instance.pk:
+                reservations = reservations.exclude(pk=self.instance.pk)
+            if reservations.count() >= flight.airplane.capacity:
+                raise ValidationError("O voo já atingiu a capacidade máxima de reservas.")
+            
+            return cleaned_data
 
 class AirplaneForm(forms.ModelForm):
     class Meta:
